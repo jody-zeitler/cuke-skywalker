ee21c2c2dd358ae30e40593517b65b8bee9dc81d
Jody Zeitler <jzeitler@redbrickhealth.com>
2018-09-25 09:33:09 -0500

    Add a single scenario test.

--- features/support/hooks.js
+++ features/support/hooks.js
@@ -1,8 +1,9 @@
 const {defineSupportCode, Status} = require('cucumber');
 
 defineSupportCode(function ({After}) {
-	After({timeout: 10 * 1000}, async function ({result}) {
-		if (result.status === Status.FAILED) {
+	After({timeout: 10 * 1000}, async function ({result, status}) {
+		status = status || result.status;
+		if (status === Status.FAILED) {
 			const screenshot = await this.driver.takeScreenshot();
 			await this.attach(screenshot, 'image/png');
 		}
